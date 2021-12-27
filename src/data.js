d3a76f5f6b2a23f39705cde89f4138951caab502
Kaleb Goad <goad.kaleb@gmail.com>
2021-12-26 20:08:02 -0600

    first commit

---
+++ src/data.js
@@ -0,0 +1,27 @@
+
+import {default as Hyabusa} from './assets/Hyabusa Gloves.png';
+import {default as Mitts} from './assets/Everlast Mitts.png';
+import {default as HeadGear} from './assets/HeadGear.png';
+import {default as Mouth} from './assets/MouthGuard w case.png';
+import {default as BodyG} from './assets/Title BodyGuard.png';
+import {default as Thai} from './assets/Title Muay Thai pads.png';
+import {default as Shin} from './assets/Title Shinguards.png';
+import {default as Speed} from './assets/Title SpeedBag.png';
+import {default as Venom} from './assets/Venom boxing gloves.jpg';
+import {default as Wraps} from './assets/Wraps.png';
+
+
+export const inventoryData = [
+    { name: 'Hyabusa Gloves', price: 23.99, img: Hyabusa, category: 'Hands' },
+    { name: 'Everlast Mitts', price: 45.99, img: Mitts, category: 'Hands' },
+    { name: 'Title Head Gear', price: 17.99, img: HeadGear, category: 'Guard' },
+    { name: 'Champs MouthGuard', price: 17.99, img: Mouth, category: 'Guard' },
+    { name: 'Title BodyGuard', price: 17.99, img: BodyG, category: 'Guard' },
+    { name: 'Muay Thai Pads', price: 49.99, img: Thai, category: 'Hands' },
+    { name: 'Title ShinGuards', price: 49.99, img: Shin, category: 'Guard' },
+    { name: 'Title SpeedBag', price: 49.99, img: Speed, category: 'Hands' },
+    { name: 'Venom boxing gloves', price: 49.99, img: Venom, category: 'Hands' },
+    { name: 'RDX Hand Wraps', price: 49.99, img: Wraps, category: 'Hands' },
+  ];
+  
+  export const currenciesData = ['USD', 'EUR', 'CAD'];
---
+++ src/features/cart/CartPanel.js
@@ -0,0 +1,22 @@
+// import React from 'react';
+// import { Cart } from './Cart';
+
+
+// export default function CartPanel ({ state, dispatch }: props) {
+//     const { state, dispatch } = props;
+
+//     return (
+//         <div>
+//         <button>^</button>
+//         <Cart 
+//             cart={state.cart}
+//             currencyFilter={state.currencyFilter}
+//             dispatch={dispatch}
+//         />
+        
+        
+        
+        
+//         </div>
+//     )
+// } 
